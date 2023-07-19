fa21287ce449db33b7b7959d6a72d624235583e8
tnyarko1 <139938937+tnyarko1@users.noreply.github.com>
2023-07-19 03:41:25 +0000

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,15 @@
+## Finding the inverse of a given matrix
+## 
+
+## Creating a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+
+} 
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}  
